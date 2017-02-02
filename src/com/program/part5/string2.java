566a9a01caf498c85b3578c5e47cc63758dbde78
gitgeekmj <bjmajie41@gmail.com>
2017-02-02 16:38:23 +0800

    solution

--- src/com/program/part5/string2.java
+++ src/com/program/part5/string2.java
@@ -21,7 +21,7 @@ public class string2 {
 		A1.right = A3;
 		A3.left = A6;
 		A3.right = A7;
-		TreeNode B2 = new TreeNode(1);
+		TreeNode B2 = new TreeNode(2);
 		TreeNode B4 = new TreeNode(4);
 		TreeNode B5 = new TreeNode(5);
 		TreeNode B8 = new TreeNode(8);
@@ -30,69 +30,56 @@ public class string2 {
 		B4.left = B8;
 		B2.right = B5;
 		B5.right = B9;
-		System.out.println(chkIdentical(A1,B2));
+		System.out.println(chkIdentical(A1, B2));
+		System.out.println(HasSubtree(A1, B2));
 	}
-    public static boolean chkIdentical(TreeNode A, TreeNode B) {
-        ArrayList<TreeNode> arr1 = new ArrayList<TreeNode>();
-        ArrayList<TreeNode> arr2 = new ArrayList<TreeNode>();
-        preOrderRecur(A,arr1);
-        preOrderRecur(B,arr2);
-        int[] tn1 = new int[arr1.size()];
-        int[] tn2 = new int[arr2.size()];
-        for(int i = 0; i < tn1.length; i++){
-        	tn1[i] = arr1.get(i).val;
-        }
-        for(int i = 0; i < tn2.length; i++){
-        	tn2[i] = arr2.get(i).val;
-        }
-        int i = 0;
-        int j = 0;
-        boolean tn = false;
-        while(i < tn1.length){
-        	if(j < tn2.length && tn1[i] == tn2[j]){
-        		while(j < tn2.length){
-        			if(i >= tn1.length){
-        				return false;
-        			}
-        			if(tn1[i] == tn2[j]){
-        				tn = true;
-        				i++;
-        				j++;
-        			}else{
-        				j = 0;
-        				break;
-        			}
-        		}
-        	}
-        	i++;
-        }
-    	return tn;
-    }
-    public static void preOrderRecur(TreeNode head,ArrayList<TreeNode> arr){
-    	if(head == null){
-    		return;
-    	}
-    	arr.add(head);
-    	preOrderRecur(head.left,arr);
-    	preOrderRecur(head.right,arr);
-    }
-    /*
-     *     
-	public boolean chkIdentical(TreeNode A, TreeNode B) {
-        // write code here
-        String t1Str = serialByPre(A);
-        String t2Str = serialByPre(B);
-        return t1Str.contains(t2Str);
-    }
-     private static String serialByPre(TreeNode head) {
-        if(head == null){
-            return "#!";
-        }
-          
-        String res = head.val + "!";
-        res += serialByPre(head.left);
-        res += serialByPre(head.right);
-        return res;
-    }
-     */
+	/**
+	 * 
+	 * @param A
+	 * @param B
+	 * @return
+	 * chkIdentical为严格判断二叉树子树型方法
+	 */
+	public static boolean chkIdentical(TreeNode A, TreeNode B) {
+		String t1Str = serialByPre(A);
+		String t2Str = serialByPre(B);
+		return t1Str.contains(t2Str);
+	}
+
+	private static String serialByPre(TreeNode head) {
+		if (head == null) {
+			return " ";
+		}
+		String res = head.val + "";
+		res += serialByPre(head.left);
+		res += serialByPre(head.right);
+		return res;
+	}
+	/**
+	 * 
+	 * @param root1
+	 * @param root2
+	 * @return
+	 * HasSubtree为判断二叉树子结构方法   只能按部就班
+	 */
+	public static boolean HasSubtree(TreeNode root1, TreeNode root2) {
+		if(root1 == null || root2 == null){
+			return false;
+		}
+		return isSubTree(root1,root2) || HasSubtree(root1.left,root2) || HasSubtree(root1.right,root2);
+	}
+	public static boolean isSubTree(TreeNode root1,TreeNode root2){
+		if(root2 == null){
+			return true;
+		}
+		if(root1 == null){
+			return false;
+		}
+		if(root2.val == root1.val){
+			return isSubTree(root1.left,root2.left) && isSubTree(root1.right,root2.right);
+		}else{
+			return false;
+		}
+	}
+
 }
