f8a5de2b95610f333222860773d370b882f1043b
gitgeekmj <bjmajie41@gmail.com>
2017-02-03 09:23:10 +0800

    string4

---
+++ src/com/program/part5/string4.java
@@ -0,0 +1,13 @@
+package com.program.part5;
+
+public class string4 {
+	public static void main(String[] args) {
+		String a = "cdab";
+		String b = "abcd";
+		System.out.println(chkRotation(a,4,b,4));
+	}
+    public static boolean chkRotation(String A, int lena, String B, int lenb) {
+    	A = A + A;
+        return A.contains(B);
+    }
+}
