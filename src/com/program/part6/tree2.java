3b05102e45e5725904f5e8ac7b51dd6af2215844
gitgeekmj <bjmajie41@gmail.com>
2017-02-04 19:57:15 +0800

    tree2

---
+++ src/com/program/part6/tree2.java
@@ -0,0 +1,29 @@
+package com.program.part6;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class tree2 {
+    public int[][] convert(TreeNode root) {
+    	ArrayList<Integer> arr = new ArrayList<Integer>();
+    	if(root != null){
+    		Stack<TreeNode> st = new Stack<TreeNode>();
+    		st.push(root);
+    		while(!st.isEmpty()){
+    			root = st.pop();
+    			arr.add(root.val);
+    			if(root.right != null){
+    				st.push(root.right);
+    			}
+    			if(root.left != null){
+    				st.push(root.left);
+    			}
+    		}
+    	}
+    	int[][] a = new int[3][arr.size()];
+    	for(int i = 0; i < arr.size(); i++){
+    		a[0][i] = arr.get(i);
+    	}
+    	return a;
+    }
+}
