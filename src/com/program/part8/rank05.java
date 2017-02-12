8ecc5be2daafc84231e3bffd29e07e994eca4ecd
gitgeekmj <bjmajie41@gmail.com>
2017-02-12 10:11:28 +0800

    rank05

---
+++ src/com/program/part8/rank05.java
@@ -0,0 +1,15 @@
+package com.program.part8;
+
+public class rank05 {
+    public int countLegalWays(int n) {
+        int res1 = 1;
+        int res2 = 1;
+        for(int i = 2*n; i > n; i--){
+        	res1 *= i;
+        }
+        for(int i = n; i > 1; i--){
+        	res2 *= i;
+        }
+        return (res1/res2)/(n+1);
+    }
+}
